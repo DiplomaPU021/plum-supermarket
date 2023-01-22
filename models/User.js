976a17e414db344ae63abce4f991a40321a6b030
OxyFoxy10 <oximuzyka@gmail.com>
2023-01-22 18:35:50 +0200

    commit auth2

---
+++ models/User.js
@@ -0,0 +1,72 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: "Please enter your full name",
+    },
+    email: {
+        type: String,
+        required: "Please enter your email",
+        trim: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: "Please enter your password",
+    },
+    role: {
+        type: String,
+        default: "user",
+    },
+    image: {
+        type: String,
+        default: "profile.gif",
+    },
+    emailVerified: {
+        type: Boolean,
+        default: false,
+    },
+    defaultPaymentMethod: {
+        type: String,
+        default: "",
+    },
+    adress: [
+        {
+            firstName: {
+                type: String,
+            },
+            lastName: {
+                type: String,
+            },
+            phoneNumber: {
+                type: String,
+            },
+            address: {
+                type: String,
+            },
+            region: {
+                type: String,
+            },
+            city: {
+                type: String,
+            },
+            zipCode: {
+                type: String,
+            },
+            country: {
+                type: String,
+            },
+            active: {
+                type: Boolean,
+                default: false,
+            },
+        },
+    ],
+},
+    { timestamps: true, }
+
+);
+const User = mongoose.model("User", userSchema) || mongoose.models("User", userSchema);
+
+export default User;
--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,2 +1,47 @@
 import nc from 'next-connect';
-import {validateEmail} from '../lib/utils';
+import bcrypt from "bcryptjs";
+import { validateEmail } from "../../../utils/validation";
+import db from "../../../utils/db";
+import User from "../../../models/User";
+import { createActivationToken } from '../../../utils/tokens';
+
+const handler = nc();
+handler.post(async (req, res) => {
+    try {
+        await db.connectDb();
+        const { name, email, password } = req.body;
+        if (!name || !email || !password) {
+            return res.status(400).json({ message: "Please fill all fields" });
+        }
+        if (!validateEmail(email)) {
+            return res.status(400).json({ message: "Please enter a valid email" });
+        }
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(200).json({ message: "This email is already registered!" }).json
+        }
+        if (password.length < 6) {
+            return res.status(400).json({ message: "Password must be at least 6 characters long" });
+        }
+        const cryptedPassword = await bcrypt.hash(password, 12);
+
+        const newUser = new User({
+            name,
+            email,
+            password: cryptedPassword
+        });
+        const addedUser = await newUser.save();
+        const activation_token = createActivationToken({
+            id: addedUser._id.toString(),
+        });
+        console.log(activation_token);
+        res.send(activation_token);
+        console.log(addedUser);
+        await db.disconnectDb();
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+
+    }
+});
+export default handler;
