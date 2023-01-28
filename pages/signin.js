5d53f9a1547e42c5f997b890bec3d1bb5c167d5d
Foxy <91662683+OxyFoxy10@users.noreply.github.com>
2023-01-28 22:27:08 +0200

    Update signin.js

--- pages/signin.js
+++ pages/signin.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import Header from "../components/header"
 import Footer from "../components/footer"
 import styles from "../styles/signin.module.scss"
