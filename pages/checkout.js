1282f30e5abd63c1ff6c3289e513c960397a3a9b
OxyFoxy10 <oximuzyka@gmail.com>
2023-02-20 21:35:41 +0200

    fixed checkoutBranch2002

--- pages/checkout.js
+++ pages/checkout.js
@@ -42,7 +42,7 @@ export async function getServerSideProps(context) {
         if (!cart) {
           return {
             redirect: {
-              destination: "/",
+              destination: "/checkout",
             }
           }
         }
