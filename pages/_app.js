a55e5daeb1943668d2f48444b1a78363f61564a5
OxyFoxy10 <oximuzyka@gmail.com>
2023-05-15 22:13:07 +0300

    crossOrigin camelCase fixed

--- pages/_app.js
+++ pages/_app.js
@@ -25,7 +25,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
         <link rel="icon" href="/favicon.ico" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap"
           rel="stylesheet"
