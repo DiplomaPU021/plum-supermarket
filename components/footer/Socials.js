64c282601839f4aae35f200240c0ead55d30a8d0
OxyFoxy10 <oximuzyka@gmail.com>
2023-05-19 18:07:21 +0300

    fixed quotes in checkoutorder info  and target blank in  footer social

--- components/footer/Socials.js
+++ components/footer/Socials.js
@@ -12,7 +12,7 @@ export default function Socials() {
                 <ul>
                     <li>
                         <div>
-                            <a href="https://www.facebook.com/" target="_blank" rel="noopener">
+                            <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
                                 <FacebookIcon fillColor={"#220F4B"} />
                             </a>
 
@@ -20,21 +20,21 @@ export default function Socials() {
                     </li>
                     <li>
                         <div>
-                            <a href="https://www.instagram.com/accounts/login/" target="_blank" rel="noopener">
+                            <a href="https://www.instagram.com/accounts/login/" target="_blank" rel="noreferrer">
                                 <InstagramIcon fillColor={"#220F4B"} />
                             </a>
                         </div>
                     </li>
                     <li>
                         <div>
-                            <a href="https://www.youtube.com/" target="_blank" rel="noopener">
+                            <a href="https://www.youtube.com/" target="_blank" rel="noreferrer">
                                 <YoutubeIcon fillColor={"#220F4B"} />
                             </a>
                         </div>
                     </li>
                     <li>
                         <div>
-                            <a href="https://twitter.com/?lang=ua" target="_blank" rel="noopener">
+                            <a href="https://twitter.com/?lang=ua" target="_blank" rel="noreferrer">
                                 <TwitterIcon fillColor={"#220F4B"} />
                             </a>
                         </div>
